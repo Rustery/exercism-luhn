c76e26603e57fff8d33d23340f60ecc8a3082ddf
Rustam <rustery@gmail.com>
2020-06-03 18:19:27 +0300

    removed extra function and modified the main loop

--- luhn.go
+++ luhn.go
@@ -2,26 +2,26 @@
 package luhn
 
 import (
-	"errors"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
 // Valid — Determine whether or not number string is valid per the Luhn formula.
 func Valid(input string) bool {
-	cleanInput, err := clearInput(input)
-	if err != nil {
+	cleanInput := strings.ReplaceAll(input, " ", "")
+	if len(cleanInput) <= 1 {
 		return false
 	}
-	reversedInput := reverse(cleanInput)
 	sum := 0
-	for k, val := range reversedInput {
-		v, _ := strconv.Atoi(string(val))
-		if k%2 == 0 {
-			sum += int(v)
+	for i, isSecondDigit := len(cleanInput), true; i > 0; i, isSecondDigit = i-1 , !isSecondDigit {
+		v, err := strconv.Atoi(string(cleanInput[i-1]))
+		if err != nil {
+			return false
+		}
+		if isSecondDigit {
+			sum += v
 		} else {
-			doubleNum := int(v) * 2
+			doubleNum := v * 2
 			if doubleNum > 9 {
 				sum += doubleNum - 9
 			} else {
@@ -33,27 +33,4 @@ func Valid(input string) bool {
 		return true
 	}
 	return false
-}
-
-func reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
-func clearInput(input string) (string, error) {
-	cleanInput := strings.Replace(input, " ", "", -1)
-	reg, err := regexp.Compile("[^0-9]+")
-	if err != nil {
-		return "", err
-	}
-	if reg.MatchString(cleanInput) {
-		return "", errors.New("Strings can contain only numbers and spaces")
-	}
-	if len(cleanInput) <= 1 {
-		return "", errors.New("Strings of length 1 or less are not valid")
-	}
-	return cleanInput, nil
 }
