2c45b64340bc0f6c914b451824b8b83ad75a81c1
Rustam <rustery@gmail.com>
2020-06-04 15:31:09 +0300

    loop refactoring

--- luhn.go
+++ luhn.go
@@ -8,29 +8,26 @@ import (
 
 // Valid — Determine whether or not number string is valid per the Luhn formula.
 func Valid(input string) bool {
-	cleanInput := strings.ReplaceAll(input, " ", "")
-	if len(cleanInput) <= 1 {
+	input = strings.ReplaceAll(input, " ", "")
+	if len(input) <= 1 {
 		return false
 	}
 	sum := 0
-	for i, isSecondDigit := len(cleanInput), true; i > 0; i, isSecondDigit = i-1 , !isSecondDigit {
-		v, err := strconv.Atoi(string(cleanInput[i-1]))
+	isSecondDigit := len(input)%2 == 0
+	for _, r := range input {
+		v, err := strconv.Atoi(string(r))
 		if err != nil {
 			return false
 		}
 		if isSecondDigit {
-			sum += v
-		} else {
-			doubleNum := v * 2
-			if doubleNum > 9 {
-				sum += doubleNum - 9
-			} else {
-				sum += doubleNum
+			v *= 2
+			if v > 9 {
+				v -= 9
 			}
 		}
+		sum += v
+		isSecondDigit = !isSecondDigit
 	}
-	if sum%10 == 0 {
-		return true
-	}
-	return false
+	return sum%10 == 0
 }
+
